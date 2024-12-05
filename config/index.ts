f6133db27adff7cc4b884eb79872e3b8bf1ffbbc
Zane_0630 <yzremail@163.com>
2024-12-05 22:32:26 +0800

    config change

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'en',
+  title: 'TaiSha Chat',
+  description: '关于太傻的一切',
+  copyright: 'Zane',
+  privacy_policy: 'None',
+  default_language: 'zh',
 }
 
 export const isShowPrompt = false
